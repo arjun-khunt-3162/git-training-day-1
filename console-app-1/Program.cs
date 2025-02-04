628ca6ee121de51dec432fe1f75cbe462afe8e8e
Arjun Khunt <arjun.khunt@matrixcomsec.com>
2025-02-04 06:42:26 +0000

    Add initial console application with multithreading example

---
+++ console-app-1/Program.cs
@@ -0,0 +1,17 @@
+﻿Thread thread1 = new(new ThreadStart(HighCpuUsageTask));
+Thread thread2 = new(new ThreadStart(HighCpuUsageTask));
+
+thread1.Start();
+thread2.Start();
+
+thread1.Join();
+thread2.Join();
+
+static void HighCpuUsageTask()
+{
+    for (int i = 0; i < 1000000000; i++)
+    {
+        // Simulate high CPU usage
+        double result = Math.Sqrt(i);
+    }
+}
